a506a8c104ce781bf34691e882161f17366a3c2e
Denis <UndeadDen@gmail.com>
2018-04-20 09:06:43 +0400

    Доделал тест

--- src/Main.java
+++ src/Main.java
@@ -1,42 +1,54 @@
 import humanResources.Department;
 import humanResources.Employee;
+import humanResources.Organization;
 
 import java.util.Scanner;
 
 public class Main {
     private static Employee employee1 = new Employee("Вася", "Пупкин");
     private static Employee employee2 = new Employee("Денис", "Крикушин", "Студент", 1500);
-    private static Employee employee3 = new Employee("Николай", "Пупкин", "Директор", 9999);
-    private static Employee employee4 = new Employee("", "");
+    private static Employee employee3 = new Employee("Николай", "Пупкин", "Директор", 99999);
+    private static Employee employee4 = new Employee("Иван", "Иванов", "Уборщик", 10000);
+    private static Employee employee5 = new Employee("", "");
     private static Employee[] employees1 = {employee1, employee2, employee3};
+    private static Employee[] employees2 = {employee4};
     private static Department department1 = new Department("Тестовый департамент", employees1);
-    private static Department[] departments1 = {department1};
+    private static Department department2 = new Department("Ещё один департамент", employees2);
+    private static Department[] departments = {department1};
+    private static Organization organization = new Organization("Тестовая организация", departments);
 
     private static void printEmployee(Employee employee) {
-        System.out.printf("Имя: %s   Фамилия: %s  Должность: %s  Зарплата: %d", employee.getFirstName(), employee.getSecondName(), employee.getJobTitle(), employee.getSalary());
+        System.out.printf("Имя: %s   Фамилия: %s  Должность: %s  Зарплата: %d \n", employee.getFirstName(), employee.getSecondName(), employee.getJobTitle(), employee.getSalary());
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Какой тест запустить (1/2/3)?");
+        System.out.println("Какой тест запустить (1/2)?");
         int i = sc.nextInt();
         if (i == 1) {
             System.out.println("Нужно больше рабочих");
             System.out.print("Имя: ");
-            employee4.setFirstName(sc.nextLine());
+            employee5.setFirstName(sc.nextLine());
             System.out.print("Фамилия: ");
-            employee4.setSecondName(sc.nextLine());
+            employee5.setSecondName(sc.nextLine());
             System.out.print("Должность: ");
-            employee4.setJobTitle(sc.nextLine());
+            employee5.setJobTitle(sc.nextLine());
             System.out.print("Зарплата: ");
-            employee4.setSalary(sc.nextInt());
-            department1.addEmployee(employee4);
-            printEmployee(employee4);
+            employee5.setSalary(sc.nextInt());
+            department1.addEmployee(employee5);
+            printEmployee(employee5);
         }
-        if (i == 2) {
-
-        } else {
-
+        else {
+            organization.addDepartment(department2);
+            System.out.printf("В организации находится %d департаментов\n", organization.getSize());
+            System.out.printf("В организации находится %d работников\n", organization.employeesQuantity());
+            System.out.printf("В организации находится %d работников на должности Директор\n", organization.employeesQuantity("Директор"));
+            System.out.printf("Наибольшую зарплату имеет %s %s в размере %d \n", organization.bestEmployee().getFirstName(), organization.bestEmployee().getSecondName(), organization.bestEmployee().getSalary());
+            organization.fireDepartment(organization.getDepartment("Тестовый департамент").getName());
+            System.out.printf("В организации находится %d департаментов\n", organization.getSize());
+            System.out.printf("В организации находится %d работников\n", organization.employeesQuantity());
+            System.out.printf("В организации находится %d работников на должности Директор\n", organization.employeesQuantity("Директор"));
+            System.out.printf("Наибольшую зарплату имеет %s %s в размере %d \n", organization.bestEmployee().getFirstName(), organization.bestEmployee().getSecondName(), organization.bestEmployee().getSalary());
         }
     }
 }
